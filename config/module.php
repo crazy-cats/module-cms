9b8517e69421e2dd4fe54a2749b839cce1cb30da
Bruce <bruce.z@infinitytesting.com.au>
2018-11-07 18:38:29 +0800

    ^ build CrazyCat CMS module

---
+++ config/module.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copyright © 2018 CrazyCat, Inc. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+
+/**
+ * @category CrazyCat
+ * @package CrazyCat\Cms
+ * @author Bruce Z <152416319@qq.com>
+ * @link http://crazy-cat.co
+ */
+return [
+    'depends' => []
+];
